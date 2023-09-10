920203cd8f8e611988e407f65a1749e698121b75
Yonatan Kra <me@yonatankra.com>
2023-09-10 21:51:04 +0300

    Add the DOM components

--- src/components/greeter/greeter.spec.ts
+++ src/components/greeter/greeter.spec.ts
@@ -11,4 +11,28 @@ describe('greeter', () => {
         expect(greeter.shadowRoot).toBeDefined();
         expect(greeter.shadowRoot?.mode).toBe('open');
     });
+
+
+    it ('should have a shadow root with mode open', () => {
+        const greeter = document.createElement('yag-greeter');
+        expect(greeter.shadowRoot?.mode).toBe('open');
+    });
+
+    it ('should have greet button inside', () => {
+        const greeter = document.createElement('yag-greeter');
+        expect(greeter.shadowRoot?.querySelector('#greet-form button')).toBeTruthy();
+    });
+
+
+    it ('should have greet form inside', () => {
+        const greeter = document.createElement('yag-greeter');
+        expect(greeter.shadowRoot?.querySelector('#greet-form')).toBeTruthy();
+    });
+
+    it ('should have greet input inside', () => {
+        const greeter = document.createElement('yag-greeter');
+        const greetInput = greeter.shadowRoot?.querySelector('#greet-input');
+        expect(greetInput).toBeTruthy();
+        expect(greetInput?.form).toEqual(greeter.shadowRoot?.querySelector('#greet-form'));
+    });
 });
--- src/components/greeter/greeter.ts
+++ src/components/greeter/greeter.ts
@@ -1,6 +1,19 @@
+export const template = `
+                <form class="row" id="greet-form">
+                    <input id="greet-input" placeholder="Enter a name..." />
+                    <button type="submit">Greet</button>
+                </form>
+        
+                <p id="greet-msg"></p>
+        `;
+const templateElement = document.createElement('template');
+templateElement.innerHTML = template;
+
 export class Greeter extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
+    const templateHTML = templateElement.content.cloneNode(true);
+    this.shadowRoot?.appendChild(templateHTML);
   }
 }
