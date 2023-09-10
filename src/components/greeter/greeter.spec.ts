959bd263a0545151363b3505e3900f4c79a3f30d
Yonatan Kra <me@yonatankra.com>
2023-09-10 21:56:31 +0300

    Copy the logic from main

--- src/components/greeter/greeter.spec.ts
+++ src/components/greeter/greeter.spec.ts
@@ -1,3 +1,4 @@
+import { mockIPC } from '@tauri-apps/api/mocks';
 import { Greeter } from './index';
 
 describe('greeter', () => {
@@ -35,4 +36,30 @@ describe('greeter', () => {
         expect(greetInput).toBeTruthy();
         expect(greetInput?.form).toEqual(greeter.shadowRoot?.querySelector('#greet-form'));
     });
+
+
+    it('should set the greeting message inside the message element', async () => {
+        const greeter = document.createElement('yag-greeter');
+        const name = 'John Doe';
+        const greetForm = greeter.shadowRoot?.querySelector('#greet-form') as HTMLFormElement;
+        const greetInput = greeter.shadowRoot?.querySelector('#greet-input') as HTMLInputElement;
+        const greetMsgEl = greeter.shadowRoot?.querySelector('#greet-msg') as HTMLElement;
+
+        greetInput.value = name;
+    
+        mockIPC((cmd, args) => {
+          if(cmd === "greet") {
+              return `Hello, ${args.name}! You've been greeted from Rust!`;
+          }
+        });
+    
+        await dispatchFormSubmit(greetForm);
+        
+        expect(greetMsgEl?.textContent).toBe(`Hello, ${name}! You've been greeted from Rust!`);
+    });
 });
+
+const dispatchFormSubmit = async (greetForm: HTMLFormElement) => {
+    greetForm.dispatchEvent(new Event("submit"));
+    await new Promise((resolve) => setTimeout(resolve, 0));
+}
--- src/components/greeter/greeter.ts
+++ src/components/greeter/greeter.ts
@@ -1,3 +1,5 @@
+import { invoke } from "@tauri-apps/api";
+
 export const template = `
                 <form class="row" id="greet-form">
                     <input id="greet-input" placeholder="Enter a name..." />
@@ -10,10 +12,22 @@ const templateElement = document.createElement('template');
 templateElement.innerHTML = template;
 
 export class Greeter extends HTMLElement {
+  #greet = async () => {
+      const greetInputEl = this.shadowRoot?.querySelector('#greet-input') as HTMLInputElement;
+      const greetMsgEl = this.shadowRoot?.querySelector('#greet-msg') as HTMLElement;
+      greetMsgEl.textContent = await invoke('greet', {
+          name: greetInputEl.value,
+      });
+  };
+
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
     const templateHTML = templateElement.content.cloneNode(true);
     this.shadowRoot?.appendChild(templateHTML);
+    this.shadowRoot?.querySelector('#greet-form')?.addEventListener('submit', (e) => {
+      e.preventDefault();
+      this.#greet();
+    });
   }
 }
