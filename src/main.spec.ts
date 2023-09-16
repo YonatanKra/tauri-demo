336fe476ca5527bc74a43f14517b550d8c06347c
Yonatan Kra <me@yonatankra.com>
2023-09-16 16:12:27 +0300

    Main now sets the yag-app in the DOM

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -4,5 +4,9 @@ describe('main', () => {
   it ('should define yag-greeter', () => {
     expect(customElements.get('yag-greeter')).toBeDefined();
   });
+
+  it('should set yag-app inside the body', () => {
+    expect(document.body.innerHTML).toBe('<yag-app></yag-app>');
+  });
 });
 
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,7 @@ import { Greeter } from "./components/greeter";
 
 customElements.define("yag-greeter", Greeter);
 
+document.body.innerHTML = "<yag-app></yag-app>";
+
+
+
